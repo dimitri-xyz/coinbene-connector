32b93bf2cf05fcd0bdd54b300fb80263e1635524
Dimitri DeFigueiredo <defigueiredo@ucdavis.edu>
2019-02-15 14:13:45 -0200

    WIP: Implement producer - needs tests

--- src/Coinbene/Producer.hs
+++ src/Coinbene/Producer.hs
@@ -35,9 +35,7 @@ producer
 producer interval config proxy state handler = do
     bkThread <- async bookThread
     link bkThread
-    -- This thread must cancel the inner bookThread if it receives an exception.
-    finally detectThread (cancel bkThread) 
-
+    finally detectThread (cancel bkThread) -- This thread must cancel the inner bookThread if it receives an exception.
   where
     bookThread = forever $ do
         book <- intoIO (C.getBook config (Proxy :: Proxy (C.Price p')) (Proxy :: Proxy (C.Vol v')) :: m (C.QuoteBook p' v'))
@@ -64,83 +62,78 @@ producer interval config proxy state handler = do
 
             closePairs <- atomicallyUpdateConnector (updateIfNewer newInfo oid (updateClose mcoid newInfo)) state
 
-            -- putStrLn $ "{{{{{{{{{{" <> show (fst <$> closePairs) <> "}}}}}}}}}}"
+            -- putStrLn $ "{{{{{ orders to be closed {{{{{" <> show (fst <$> closePairs) <> "}}}}}}}}}}"
 
             dispatch $ concat (snd <$> closePairs)
             forM_ (fst <$> closePairs) (\oid -> atomicallyUpdateConnector (removeEntry oid) state)
 
             -- endState <- readTVarIO state
-            -- putStrLn $ "<<<<<<<<<<" <> show endState <> ">>>>>>>>>>"
+            -- putStrLn $ "<<<<< state at end of cycle <<<<<" <> show endState <> ">>>>>>>>>>"
 
         threadDelay interval
 
-    atomicallyUpdateConnector :: State CoinbeneConnector a -> TVar CoinbeneConnector -> IO a
-    atomicallyUpdateConnector updater connector = atomically $ stateTVar connector $ runState updater
+--------------------------------------------------------------------------------
+atomicallyUpdateConnector :: State CoinbeneConnector a -> TVar CoinbeneConnector -> IO a
+atomicallyUpdateConnector updater connector = atomically $ stateTVar connector $ runState updater
 
-    updateIfNewer :: C.OrderInfo -> C.OrderID -> State ConnectorOrderInfo [a] -> State CoinbeneConnector [a]
-    updateIfNewer newInfo oid updater = do
-        connectorMap <- get 
-        let mCurInfo = lookupMain oid connectorMap
-        case mCurInfo of
-            Nothing -> return [] -- order with current orderID has disappeared, skip its update in this polling cycle
-            Just (mCoid, curInfo) -> do
-                let newTime = fromMaybe (C.created newInfo) (C.mModified newInfo)
-                    curTime = fromMaybe 0                   (mModified   curInfo) -- force update if missing
-                if newTime < curTime -- sanity test
-                    then return [] -- order info returned is stale, skip order update
-                    else do
-                        let (as, updatedEntry) = runState updater curInfo   -- updateOrder newInfo curInfo
-                        put (adjustMain (const updatedEntry) oid connectorMap)
-                        return as
+updateIfNewer :: C.OrderInfo -> C.OrderID -> State ConnectorOrderInfo [a] -> State CoinbeneConnector [a]
+updateIfNewer newInfo oid updater = do
+    connectorMap <- get
+    let mCurInfo = lookupMain oid connectorMap
+    case mCurInfo of
+        Nothing -> return [] -- order with current orderID has disappeared, skip its update in this polling cycle
+        Just (mCoid, curInfo) -> do
+            let newTime = fromMaybe (C.created newInfo) (C.mModified newInfo)
+                curTime = fromMaybe 0                   (mModified   curInfo) -- force update if missing
+            if newTime < curTime -- sanity test
+                then return [] -- order info returned is stale, skip order update
+                else do
+                    let (as, updatedEntry) = runState updater curInfo
+                    put (adjustMain (const updatedEntry) oid connectorMap)
+                    return as
 
-    isClosingEv :: TradingEv p v q c -> Bool
-    isClosingEv (CancelEv _) = True
-    isClosingEv (DoneEv   _) = True
-    isClosingEv _            = False
+--------------------------------------------------------------------------------
+isClosingEv :: TradingEv p v q c -> Bool
+isClosingEv (CancelEv _) = True
+isClosingEv (DoneEv   _) = True
+isClosingEv _            = False
 
-    removeEntry :: C.OrderID -> State CoinbeneConnector ()
-    removeEntry oid = do
-        dict <- get
-        put (deleteMain oid dict)
+removeEntry :: C.OrderID -> State CoinbeneConnector ()
+removeEntry oid = do
+    dict <- get
+    put (deleteMain oid dict)
 
--- data OrderInfo =
---     LimitOrder
---     { market     :: String
---     , oSide      :: OrderSide
---     , limitPrice :: Price Scientific
---     , limitVol   :: Vol   Scientific
---     , orderID    :: OrderID
---     , created      :: MilliEpoch
---     , mModified    :: Maybe MilliEpoch
---     , status       :: OrderStatus
---     , filledVol    :: Vol  Scientific
---     , filledAmount :: Cost Scientific
---     , mAvePriceAndFees :: Maybe (Price Scientific, Cost Scientific) -- (average price, fees), nothing means "don't know"
---     }
+--------------------------------------------------------------------------------
+-- return events/updates due to fill events only
+updateFills :: (Coin p, Coin v) => Maybe ClientOID -> C.OrderInfo -> State ConnectorOrderInfo [TradingEv p v q c]
+updateFills mcoid newInfo = do
+    curInfo <- get
+    if C.filledVol newInfo <= filledVol curInfo
+        then return []
+        else do
+            put curInfo { mModified        = C.mModified        newInfo
+                        , status           = C.status           newInfo
+                        , filledVol        = C.filledVol        newInfo
+                        , filledAmount     = C.filledAmount     newInfo
+                        , mAvePriceAndFees = C.mAvePriceAndFees newInfo
+                        }
+            let fillVol   = C.filledVol newInfo - filledVol curInfo
+                fillPrice = if filledVol curInfo > 0
+                                then limitPrice curInfo -- must have been resting on the book (not immediately executed)
+                                else case C.mAvePriceAndFees newInfo of
+                                        Just (p, _) -> p -- may have immediately executed before sitting on book (but previous executed vol is zero)
+                                        Nothing     -> error "Call to get order info did not return average price, but order has executed" 
 
--- data ConnectorOrderInfo =
---     FillStatus
---     { oSide        :: C.OrderSide
---     , limitPrice   :: C.Price Scientific
---     , limitVol     :: C.Vol   Scientific
---     , mModified    :: Maybe C.MilliEpoch
---     , status       :: C.OrderStatus
---     , filledVol    :: C.Vol  Scientific
---     , filledAmount :: C.Cost Scientific
---     -- (average price, fees), nothing means "don't know"
---     , mAvePriceAndFees :: Maybe (C.Price Scientific, C.Cost Scientific)
+            return [FillsEv [FillEv (fromCB $ C.oSide newInfo) (realToFrac fillPrice) (realToFrac fillVol) mcoid]]
 
-    -- return events/updates due to fill events
-    updateFills :: Maybe ClientOID -> C.OrderInfo -> State ConnectorOrderInfo [TradingEv p v q c]
-    updateFills mcoid newInfo = return []
 
-    -- return events/oids of orders that have been `CancelEv` or `DoneEv`
-    updateClose :: Maybe ClientOID -> C.OrderInfo -> State ConnectorOrderInfo [(C.OrderID, [TradingEv p v q c])]
-    updateClose mcoid newInfo = do
-        let oid = C.orderID newInfo
-        case C.status newInfo of
-            C.PartiallyCanceled -> return [(oid, [CancelEv mcoid])]
-            C.Canceled          -> return [(oid, [CancelEv mcoid])]
-            C.Filled            -> return [(oid, [DoneEv mcoid])]
-            _                   -> return []
+-- return events/oids of orders that have been `CancelEv` or `DoneEv` (only)
+updateClose :: (Coin p, Coin v) => Maybe ClientOID -> C.OrderInfo -> State ConnectorOrderInfo [(C.OrderID, [TradingEv p v q c])]
+updateClose mcoid newInfo = do
+    let oid = C.orderID newInfo
+    case C.status newInfo of
+        C.PartiallyCanceled -> return [(oid, [CancelEv mcoid])]
+        C.Canceled          -> return [(oid, [CancelEv mcoid])]
+        C.Filled            -> return [(oid, [DoneEv mcoid])]
+        _                   -> return []
 
